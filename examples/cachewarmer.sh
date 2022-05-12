fa768dd8abe298512cde2b6d2e943fe33138f764
stefan.vanechtelt <stefan.vanechtelt@experius.nl>
2022-05-12 11:01:11 +0200

    [FEATURE][PWAI-460] Cron tabs and multi threaded data

--- examples/cachewarmer.sh
+++ examples/cachewarmer.sh
@@ -3,6 +3,11 @@
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin";
 cd /home/experius/seosnap;
 
+echo "Cachewarm Start"
+
+docker restart snapstack_rendertron
+sleep 60
+
 # Desktop cachewarm - website id = 2
 echo "Desktop cachewarm"
 make warm A='cache --recache=1 2' &&
@@ -18,3 +23,8 @@ make warm A='cache --recache=1 --mobile=True 2' &&
 
 docker restart snapstack_rendertron
 sleep 60
+
+
+# New queue method Cron(s)
+## Desktop cachewarm - website id = 2
+make thread A='cache 2 --use_queue=1 --recache=1 --follow_next=1' && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/xxxxxxxxx
